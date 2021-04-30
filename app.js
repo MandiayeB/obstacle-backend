2a2e9d58604705ceb57c2827d0d9c44c3033dccc
BADIANE Mandiaye <badiane@et.intechinfo.fr>
2021-04-30 15:27:32 +0200

    Renaming a route
    Changing "goal.route.js" to "homepage.route.js", it seems more appropriate for this route's purpose.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const PostgresClient = require('./PostgresClient');
-const goalRouter = require('./routes/goal.route');
+const homepageRouter = require('./routes/homepage.route');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,7 +10,7 @@ PostgresClient.init().then(() => console.log("Connected to Postgres Db!"));
 
 app.use(express.json());
 
-app.use('/', goalRouter);
+app.use('/', homepageRouter);
 
 app.listen(port, () => {
     console.log('Express server is up!');
