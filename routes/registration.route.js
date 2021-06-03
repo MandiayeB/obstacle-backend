6ef42d8533a474ed4af2f4a292eaddbc350e8770
Le FUR François <flefur@et.intechinfo.fr>
2021-06-03 11:04:02 +0200

    Modification on registration
    Modified name of the password confirmation in order to match the frontend changes

--- routes/registration.route.js
+++ routes/registration.route.js
@@ -3,17 +3,17 @@ const User = require('../models/user.model');
 
 router.post('/', async(req, res) => {
 
-    const { firstname,lastname,email,password,pw_confirmation,gender,birthdate } = req.body;
+    const { firstname,lastname,email,password,confirm,gender,birthdate } = req.body;
 
-    if (firstname && lastname && email && password && pw_confirmation && gender && birthdate) {
+    if (firstname && lastname && email && password && confirm && gender && birthdate) {
         console.log("We're in");
-        console.log(firstname, lastname, email, password, pw_confirmation, gender, birthdate);
+        console.log(firstname, lastname, email, password, confirm, gender, birthdate);
         if (await User.findByEmail(email)) {
             res.status(403).json({ msg: 'This email is already used.' });
 
         } else {
 
-            if (password === pw_confirmation) {
+            if (password === confirm) {
                 const createUser = await User.create(firstname, lastname, email, password, gender, birthdate, 1);
                 const credentials = await User.findByEmail(email);
                 req.session.authenticated = true;
@@ -25,7 +25,7 @@ router.post('/', async(req, res) => {
             }
         }
     } else {
-        console.log(firstname, lastname, email, password, pw_confirmation, gender, birthdate);
+        console.log(firstname, lastname, email, password, confirm, gender, birthdate);
         res.status(403).json({ msg: 'Wrong Credentials.' });
     }
 });
